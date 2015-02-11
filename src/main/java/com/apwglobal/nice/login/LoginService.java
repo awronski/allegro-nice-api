36370e23196f9d99b338f4d4543e57b2d9b4e72c
Artur Wronski <a.wronski@mgmoptima.com>
2015-02-11 09:50:22 +0100

    - password is now encrypted before transmiting to allegro

--- src/main/java/com/apwglobal/nice/login/LoginService.java
+++ src/main/java/com/apwglobal/nice/login/LoginService.java
@@ -5,11 +5,17 @@ import com.apwglobal.nice.service.AbstractService;
 import com.apwglobal.nice.service.AllegroSession;
 import com.apwglobal.nice.service.Configuration;
 import com.apwglobal.nice.util.VersionUtil;
-import pl.allegro.webapi.DoLoginRequest;
-import pl.allegro.webapi.DoLoginResponse;
+import pl.allegro.webapi.DoLoginEncRequest;
+import pl.allegro.webapi.DoLoginEncResponse;
 import pl.allegro.webapi.ServicePort;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class LoginService extends AbstractService {
+
+    private static final String SHA_256 = "SHA-256";
 
     public LoginService(ServicePort allegro, Credentials cred, Configuration conf) {
         super(allegro, cred, conf);
@@ -18,12 +24,22 @@ public class LoginService extends AbstractService {
     public AllegroSession login() {
         long version = AllegroExecutor.execute(() -> VersionUtil.getVersion(allegro, conf.getCountryId(), cred.getKey()));
 
-        DoLoginRequest request = new DoLoginRequest(cred.getUsername(), cred.getPassowrd(), conf.getCountryId(), cred.getKey(), version);
-        DoLoginResponse response = AllegroExecutor.execute(() -> allegro.doLogin(request));
+        DoLoginEncRequest request = new DoLoginEncRequest(cred.getUsername(), encPassword(cred.getPassowrd()), conf.getCountryId(), cred.getKey(), version);
+        DoLoginEncResponse response = AllegroExecutor.execute(() -> allegro.doLoginEnc(request));
 
         return new AllegroSession.Builder()
                 .sessionId(response.getSessionHandlePart())
                 .userId(response.getUserId())
                 .build();
+    }
+
+    public String encPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(SHA_256);
+            md.update(password.getBytes());
+            return Base64.getEncoder().encodeToString(md.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 }
