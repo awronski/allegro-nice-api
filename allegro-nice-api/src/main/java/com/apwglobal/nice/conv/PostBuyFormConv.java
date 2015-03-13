c60268042fe087c2c868f4aecffef74d372ef719
Artur Wronski <a.wronski@mgmoptima.com>
2015-03-13 11:00:47 +0100

    - items were added to post buy form

--- allegro-nice-api/src/main/java/com/apwglobal/nice/conv/PostBuyFormConv.java
+++ allegro-nice-api/src/main/java/com/apwglobal/nice/conv/PostBuyFormConv.java
@@ -1,15 +1,25 @@
 package com.apwglobal.nice.conv;
 
+import com.apwglobal.nice.domain.Address;
+import com.apwglobal.nice.domain.Item;
 import com.apwglobal.nice.domain.PostBuyForm;
-import pl.allegro.webapi.PostBuyFormAddressStruct;
 import pl.allegro.webapi.PostBuyFormDataStruct;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PostBuyFormConv {
 
     public static PostBuyForm convert(PostBuyFormDataStruct f) {
 
-        PostBuyFormAddressStruct shipment = f.getPostBuyFormShipmentAddress();
-        PostBuyFormAddressStruct invoice = f.getPostBuyFormInvoiceData();
+        Address shipment = AddressConv.convert(f.getPostBuyFormShipmentAddress());
+        Address invoice = AddressConv.convert(f.getPostBuyFormInvoiceData());
+
+        List<Item> items = f.getPostBuyFormItems()
+                .getItem()
+                .stream()
+                .map(ItemConv::convert)
+                .collect(Collectors.toList());
 
         return new PostBuyForm.Builder()
                 .transactionId(f.getPostBuyFormId())
@@ -23,8 +33,9 @@ public class PostBuyFormConv {
                 .payId(f.getPostBuyFormPayId())
                 .payStatus(f.getPostBuyFormPayStatus())
                 .shipmentId(f.getPostBuyFormShipmentId())
-                .invoice(AddressConv.convert(invoice))
-                .shipment(AddressConv.convert(shipment))
+                .invoice(invoice)
+                .shipment(shipment)
+                .items(items)
                 .build();
     }
 }
