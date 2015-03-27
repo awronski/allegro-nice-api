abfe30a73d79da29a184ee9316711674c6aa0830
Artur Wronski <a.wronski@mgmoptima.com>
2015-03-27 09:59:55 +0100

    - payment processed class

---
+++ allegro-nice-domain/src/main/java/com/apwglobal/nice/domain/PaymentProcessed.java
@@ -0,0 +1,78 @@
+package com.apwglobal.nice.domain;
+
+import java.util.Date;
+
+public class PaymentProcessed {
+
+    private long transactionId;
+    private boolean processed;
+    private Date date;
+    private String ref;
+
+    public PaymentProcessed() { }
+
+    private PaymentProcessed(Builder builder) {
+        transactionId = builder.transactionId;
+        processed = builder.processed;
+        date = builder.date;
+        ref = builder.ref;
+    }
+
+    public long getTransactionId() {
+        return transactionId;
+    }
+    public boolean isProcessed() {
+        return processed;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public String getRef() {
+        return ref;
+    }
+
+    public static final class Builder {
+        private long transactionId;
+        private boolean processed;
+        private Date date;
+        private String ref;
+
+        public Builder() {
+        }
+
+        public Builder transactionId(long transactionId) {
+            this.transactionId = transactionId;
+            return this;
+        }
+
+        public Builder processed(boolean processed) {
+            this.processed = processed;
+            return this;
+        }
+
+        public Builder date(Date date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder ref(String ref) {
+            this.ref = ref;
+            return this;
+        }
+
+        public PaymentProcessed build() {
+            return new PaymentProcessed(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentProcessed{" +
+                "transactionId=" + transactionId +
+                ", processed=" + processed +
+                ", date=" + date +
+                ", ref='" + ref + '\'' +
+                '}';
+    }
+
+}
