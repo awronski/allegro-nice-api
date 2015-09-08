b59ac1de6dcec3bc982bca3d4dff1fc30877cd44
Artur Wronski <a.wronski@mgmoptima.com>
2015-09-08 10:03:43 +0200

    - update becaouse of allegro api changed

--- allegro-nice-api/src/main/java/com/apwglobal/nice/client/ClientService.java
+++ allegro-nice-api/src/main/java/com/apwglobal/nice/client/ClientService.java
@@ -7,7 +7,10 @@ import com.apwglobal.nice.service.Configuration;
 import pl.allegro.webapi.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.singletonList;
 
 public class ClientService extends AbstractService {
 
@@ -16,7 +19,9 @@ public class ClientService extends AbstractService {
     }
 
     public List<ItemPostBuyDataStruct> getClientsDate(String session, long itemId) {
-        DoGetPostBuyDataRequest request = new DoGetPostBuyDataRequest(session, new ArrayOfLong(Arrays.asList(itemId)));
+        DoGetPostBuyDataRequest request = new DoGetPostBuyDataRequest();
+        request.setSessionHandle(session);
+        request.setItemsArray(new ArrayOfLong(singletonList(itemId)));
         DoGetPostBuyDataResponse response = AllegroExecutor.execute(() -> allegro.doGetPostBuyData(request));
         return response.getItemsPostBuyData().getItem();
     }
