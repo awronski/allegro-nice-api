212e49e227ffefc824bec439fa8d3dafb1fc6ca8
artur <artur@sunet.pl>
2017-08-16 14:04:52 +0200

    - SalesConditions was added

---
+++ allegro-nice-domain/src/main/java/com/apwglobal/nice/domain/SalesConditions.java
@@ -0,0 +1,27 @@
+package com.apwglobal.nice.domain;
+
+public class SalesConditions {
+
+    private String impliedWarranty;
+    private String returnPolicy;
+    private String warranty;
+
+    public SalesConditions(String impliedWarranty, String returnPolicy, String warranty) {
+        this.impliedWarranty = impliedWarranty;
+        this.returnPolicy = returnPolicy;
+        this.warranty = warranty;
+    }
+
+    public String getImpliedWarranty() {
+        return impliedWarranty;
+    }
+
+    public String getReturnPolicy() {
+        return returnPolicy;
+    }
+
+    public String getWarranty() {
+        return warranty;
+    }
+
+}
